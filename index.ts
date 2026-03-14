59ab38b78cbf3d3ac2d93cf4ed71a394fae6eb8d
ryan <ryanennns@gmail.com>
2026-03-14 13:02:20 -0400

    ts & more

---
+++ index.ts
@@ -0,0 +1,53 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const key = "servers";
+const client = createClient();
+await client.connect();
+
+/**
+ * TODO - publish an event to all wss on reset of server list,
+ * allowing them all to re-register. this way the load balancer
+ * can be restarted without needing to reboot all wss instances.
+ */
+await client.del(key);
+
+const addServer = async (name: string) => {
+  const servers = JSON.parse((await client.get(key)) ?? "[]") || [];
+  servers.push(name);
+  await client.set(key, JSON.stringify([...new Set(servers)]));
+};
+
+const removeServer = async (name: string) => {
+  const servers = JSON.parse((await client.get(key)) ?? "[]") || [];
+  const newServers = servers.filter((x: string) => x !== name);
+  await client.set(key, JSON.stringify(newServers));
+};
+
+const getServers = async () =>
+  JSON.parse((await client.get(key)) ?? "[]") || [];
+
+app.get("/get-server", async (req, res) => {
+  res.send(JSON.stringify(await getServers()));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const shutdown = async () => {
+  try {
+    await client.quit();
+  } catch {
+    client.destroy();
+  } finally {
+    process.exit(0);
+  }
+};
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
